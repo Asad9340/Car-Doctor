e5f2a832af8d889bc4c52305decabaf3b371addb
Asad9340 <asadulimran1999@gmail.com>
2024-05-03 00:47:42 +0600

    redesign logn in form

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,4 +1,8 @@
+import { FaGoogle, FaLinkedin } from 'react-icons/fa6';
 import logIn from '../../assets/images/login/login.svg';
+import { FaFacebook } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 function SignIn() {
   const handleSubmit = e => {
     e.preventDefault();
@@ -10,11 +14,11 @@ function SignIn() {
   };
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 p-4 lg:p-8">
-      <div>
+      <div className='flex justify-center items-center h-full'>
         <img src={logIn} alt="" />
       </div>
-      <div>
-        {' '}
+      <div className='border rounded-lg p-4'>
+        <h2 className="text-center text-3xl font-bold lg:text-4xl">Log In</h2>
         <form onSubmit={handleSubmit} className="card-body">
           <div className="form-control">
             <label className="label">
@@ -51,6 +55,15 @@ function SignIn() {
             </button>
           </div>
         </form>
+        <p className="text-center">Or Sign In With </p>
+        <div className="flex justify-center items-center gap-4 mt-3 text-2xl lg:text-3xl">
+          <FaFacebook className="text-blue-700" />
+          <FaLinkedin className="text-blue-500" />
+          <FaGoogle />
+        </div>
+        <p className="text-center mt-3">
+          Haven&apos;t an Account? <Link to='/signUp' className='font-semibold'>Create Account.</Link>
+        </p>
       </div>
     </div>
   );
