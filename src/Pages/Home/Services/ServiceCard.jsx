ee1eb4a1310ef251aeffb395c57408410eb5dd64
Asad9340 <asadulimran1999@gmail.com>
2024-05-05 09:03:23 +0600

    aaded some padding

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -4,7 +4,7 @@ function ServiceCard({ service }) {
   console.log(service._id);
   const { _id,img, title, price } = service;
   return (
-    <div className="space-y-3 border p-4 rounded-xl">
+    <div className="space-y-3 border p-3 md:p-4 rounded-xl">
       <img src={img} className=" h-[250px] w-full rounded-xl " alt="" />
       <h2 className="text-2xl font-bold">{title}</h2>
       <div className="flex justify-between gap-3 px-4 text-[#FF3811] font-semibold">
