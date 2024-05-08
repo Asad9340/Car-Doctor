62cf7c9ee0f65befa6d8d54d8b6f368628c0b7ad
Asad9340 <asadulimran1999@gmail.com>
2024-05-08 23:08:41 +0600

    added cart details page banner

--- src/components/CartDetails/CartDetails.jsx
+++ src/components/CartDetails/CartDetails.jsx
@@ -1,8 +1,17 @@
-
+import cartImage from '../../assets/images/checkout/checkout.png'
 function CartDetails() {
   return (
-    <div>CartDetails</div>
-  )
+    <div className='my-10 md:my-16'>
+      <div className="flex justify-center relative ">
+        <img src={cartImage} className="w-full" alt="" />
+        <div className="absolute bg-gradient-to-r from-[#151515] to-[#15151500]  h-full w-full rounded-xl">
+          <h2 className="text-3xl md:text-4xl font-semibold lg:font-bold text-white flex  items-center h-full ml-16">
+            CheckOut
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartDetails
